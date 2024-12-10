1252a85154b92ace66285cb04cd267e30631a132
seejn <seejnmaharjan@gmail.com>
2024-12-10 23:25:29 +0545

    debounce hook to prevent continuous mousemove event

---
+++ hooks/debounce.js
@@ -0,0 +1,8 @@
+const debounce = (func, delay) => {
+    let timeout;
+    return (...args) => {
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, args), delay);
+    };
+};
+export default debounce
