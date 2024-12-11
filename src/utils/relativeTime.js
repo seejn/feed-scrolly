80aade5d149277cf310eb8bf3159e8242ee92945
seejn <seejnmaharjan@gmail.com>
2024-12-11 19:12:52 +0545

    fixed singular/plural term in relativeTime

--- src/utils/relativeTime.js
+++ src/utils/relativeTime.js
@@ -8,37 +8,38 @@ const formatRelativeTime = (inputDate) => {
 
     const diffInSeconds = Math.floor((now - givenDate) / 1000);
 
+
     if (diffInSeconds < 60) {
-        return `${diffInSeconds} seconds ago`;
+        return `${diffInSeconds} ${diffInSeconds > 1 ? "seconds" : "second"} ago`;
     }
 
     const diffInMinutes = Math.floor(diffInSeconds / 60);
     if (diffInMinutes < 60) {
-        return `${diffInMinutes} minutes ago`;
+        return `${diffInMinutes} ${diffInMinutes > 1 ? "minutes" : "minute"} ago`;
     }
 
     const diffInHours = Math.floor(diffInMinutes / 60);
     if (diffInHours < 24) {
-        return `${diffInHours} hours ago`;
+        return `${diffInHours} ${diffInHours > 1 ? "hours" : "hour"} ago`;
     }
 
     const diffInDays = Math.floor(diffInHours / 24);
     if (diffInDays < 7) {
-        return `${diffInDays} days ago`;
+        return `${diffInDays} ${diffInDays > 1 ? "days" : "day"} ago`;
     }
 
     const diffInWeeks = Math.floor(diffInDays / 7);
     if (diffInWeeks < 4) {
-        return `${diffInWeeks} weeks ago`;
+        return `${diffInWeeks} ${diffInWeeks > 1 ? "weeks" : "week"} ago`;
     }
 
     const diffInMonths = Math.floor(diffInDays / 30); // Approximation
     if (diffInMonths < 12) {
-        return `${diffInMonths} months ago`;
+        return `${diffInMonths} ${diffInMonths > 1 ? "months" : "month"} ago`;
     }
 
     const diffInYears = Math.floor(diffInMonths / 12);
-    return `${diffInYears} years ago`;
+    return `${diffInYears} ${diffInYears > 1 ? "years" : "year"} ago`;
 };
 
 export default formatRelativeTime;
